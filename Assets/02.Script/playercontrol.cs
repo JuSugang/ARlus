96ff74a7866c36c9e0720dbfbbad396bfbcc4a2f
JuSugang <tnrkd7679@naver.com>
2018-03-18 11:08:26 +0900

    spline asset 변경

---
+++ Assets/02.Script/playercontrol.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using BansheeGz.BGSpline.Components;
+
+public class playercontrol : MonoBehaviour
+{
+    public GameObject line;
+    // Use this for initialization
+    void Start()
+    {
+
+
+        BGCcCursorChangeLinear a = line.GetComponent<BGCcCursorChangeLinear>();
+        a.Speed = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
